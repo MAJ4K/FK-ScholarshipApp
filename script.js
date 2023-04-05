0dfbe2fb777d65bf49b244669edccd4cedd4374b
MichaelJenkins <maj4k@yahoo.com>
2023-04-04 23:10:30 -0400

    ability to add filters and some profile changes

---
+++ script.js
@@ -0,0 +1,22 @@
+const filterGroup = document.getElementById('FGroup');
+const addfilterbtn = document.getElementById('addfltbtn');
+const flabelinp = document.getElementById('flabelinp');
+
+
+function addfilterbutton() {
+    if (!flabelinp.value) return;
+    const inp = document.createElement('input');
+    const lab = document.createElement('label');
+    const ident = "ts" + filterGroup.children.length + "fltrad";
+
+    inp.type = "radio";
+    inp.classList.add("btn-check");
+    inp.name = "fltRadio";
+    inp.id = ident;
+    lab.innerText = flabelinp.value;
+    lab.classList.add("btn");
+    lab.classList.add("btn-outline-secondary");
+    lab.htmlFor = ident
+    filterGroup.appendChild(inp);
+    filterGroup.appendChild(lab);
+}
