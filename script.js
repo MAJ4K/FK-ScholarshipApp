2ef0825775fe497a2103f1cb80836929e480e0fd
MichaelJenkins <maj4k@yahoo.com>
2023-05-31 19:15:05 -0400

    completed front-end Only factors

--- script.js
+++ script.js
@@ -1,5 +1,114 @@
+const surebtn = document.getElementById("SUREBTN");
+const surdelbtn = document.getElementById('sureModal').getElementsByTagName('input')[0];
+
 const navbtns = document.getElementById("NAVIGATION").getElementsByTagName('input');
 const maintabs = document.getElementsByTagName('main');
+
+const profileForm = document.getElementById("PROFILE");
+var profile = JSON.parse(localStorage.getItem("profile"));
+
+const schdatabtn = document.getElementById("SCHDATABTN");
+const scholarshipContainer = document.getElementById("CONTENT_CONTAINER").children;
+
+const filtermenu = document.getElementById("FILTERMENU").children;
+const filter = {
+	primaries: filtermenu[0].children[0].getElementsByTagName('input'),
+	secondaries: filtermenu[0].children[1],
+	aux: filtermenu[1].getElementsByTagName('label'),
+	form: document.getElementById("FILTERFORM"),
+	formerrdiv: document.getElementById("filterInvalidFeedback"),
+	addbtn: document.getElementById("addfltbtn"),
+	dismissbtn: document.getElementById('filterModal').getElementsByClassName('btn-close')[0]
+}
+var filterdata = {
+	id: 023,
+	filters: {}
+};
+
+for (const [key, value] of Object.entries(JSON.parse(localStorage.getItem("filters"))['filters'])) {
+	addSecondFilter(key,value);
+}
+filter.aux[0].addEventListener('click', ()=>{
+	surebtn.click();
+});
+for (const btns of filter.primaries) {
+	btns.addEventListener('click', ()=>{
+		filter.aux[0].hidden = true;
+		surdelbtn.removeEventListener('click',{});
+	});
+}
+filter.addbtn.addEventListener('click', ()=>{
+	const filtername = filter.form.getElementsByTagName('input')[0].value;
+	if (!filtername) {filter.formerrdiv.innerText = "Invalid Filter Name"; return;}
+	for (const labels of filter.secondaries.getElementsByTagName('label'))
+		if (labels.innerText == filtername){
+			filter.formerrdiv.innerText = "Filter Name Already In Use"; return;
+	}
+
+	var data = {name:filtername};
+	for (const elm of filter.form.getElementsByTagName('input')) {
+		data[elm.name] = elm.value;
+		if (elm.type == "checkbox"){data[elm.name] = Boolean(elm.checked);}
+	}
+	for (const elm of filter.form.getElementsByTagName('select')) {
+		data[elm.name] = elm.value;
+	}
+	filterdata.filters[filtername] = data;
+	localStorage.setItem("filters", JSON.stringify(filterdata));
+	
+	addSecondFilter(filtername,data);	
+	filter.dismissbtn.click();	
+});
+surdelbtn.addEventListener('click',()=>{
+	var activeinp = undefined;
+	var activelab = undefined;
+	var labfound = false;
+	for (const elmt of filter.secondaries.children){
+		if (labfound) {activelab = elmt; break;}
+		if (elmt.checked) {activeinp = elmt; labfound = true;}
+	}
+	if (!activelab) {return;}
+	const filtername = activelab.innerText;
+	activeinp.remove(); activelab.remove();
+	tempdict = {};
+	for (const [key, value] of Object.entries(filterdata.filters)) {
+		if (key == filtername) continue;
+		tempdict[key] = value;
+	}
+	filterdata.filters = tempdict;
+	localStorage.setItem("filters", JSON.stringify(filterdata));
+	filter.aux[0].hidden = true;
+	filter.primaries[0].click();
+	document.getElementById('sureModal').getElementsByTagName('button')[0].click();
+});
+function addSecondFilter(filtername,data) {
+	filterdata.id += 1;
+	const inp = document.createElement('input');
+	const lab = document.createElement('label');
+	inp.type = "radio";
+	inp.classList.add('btn-check');
+	inp.name = "filters";
+	inp.id = "flt" + filterdata.id;
+	inp.autocomplete = "off";
+	lab.classList.add('btn','btn-outline-primary');
+	lab.htmlFor = "flt" + filterdata.id;
+	lab.innerText = filtername;
+	lab.addEventListener('click', ()=>{
+		filter.aux[0].hidden = false; 
+		filterScholarships(data);
+	});
+	filter.secondaries.appendChild(inp);
+	filter.secondaries.appendChild(lab);
+}
+function filterScholarships() {}
+
+
+for (const child of scholarshipContainer) {
+	child.addEventListener('click', () => {
+		schdatabtn.click();
+	}); 
+}
+
 
 for (const btn of navbtns) {
 	btn.addEventListener('change', ()=>{
@@ -10,4 +119,48 @@ for (const btn of navbtns) {
 		const tab = document.getElementById(btn.value);
 		tab.classList.add('active');
 	});
+}
+
+restoreProfileValues();
+function editProfile() {
+	const profileInputs = profileForm.getElementsByTagName('input');
+	profileInputs[profileInputs.length - 3].hidden = true;
+	profileInputs[profileInputs.length - 2].hidden = false;
+	profileInputs[profileInputs.length - 1].hidden = false;
+	for (const inp of profileInputs) {
+		if(!inp.classList.contains('form-control')) break;
+		inp.disabled = false;
+	}
+}
+function saveProfile(save) {
+	const profileInputs = profileForm.getElementsByTagName('input');
+	profileInputs[profileInputs.length - 3].hidden = false;
+	profileInputs[profileInputs.length - 2].hidden = true;
+	profileInputs[profileInputs.length - 1].hidden = true;
+	for (const inp of profileInputs) {
+		if(!inp.classList.contains('form-control')) break;
+		inp.disabled = true;
+	}
+	const profileItems = profileForm.getElementsByTagName('div');
+	if (save) {
+		for (const item of profileItems) {
+			var itemData = [];
+			for(const dat of item.getElementsByTagName('input')){
+				itemData.push(dat.value);
+			}
+			profile[item.children[0].innerText] = itemData;
+		}
+		localStorage.setItem('profile',JSON.stringify(profile));
+	} else {restoreProfileValues();}
+}
+function restoreProfileValues() {
+	const profileItems = profileForm.getElementsByTagName('div');
+	if (Object.keys(profile).length === 0) return;
+	for (const item of profileItems) {
+		const name = item.children[0].innerText;
+		for(const i in item.getElementsByTagName('input')){
+			item.getElementsByTagName('input')[i].value = profile[name][i];
+		}
+	}
+	return true;
 }
